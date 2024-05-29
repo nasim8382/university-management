7c08545d993022d375cfc25ab5a7d509bf4d5de3
Nasim Reza <nasim8382@gmail.com>
2024-05-29 14:05:30 +0600

    user validation updated

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -5,12 +5,15 @@ const userSchema = new Schema<IUser>(
   {
     id: {
       type: String,
+      required: true,
     },
     password: {
       type: String,
+      required: true,
     },
     needsPasswordChange: {
       type: Boolean,
+      default: true,
     },
     role: {
       type: String,
@@ -19,9 +22,11 @@ const userSchema = new Schema<IUser>(
     status: {
       type: String,
       enum: ["in-progress", "blocked"],
+      default: "in-progress",
     },
     isDeleted: {
       type: Boolean,
+      default: false,
     },
   },
   {
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,15 +1,13 @@
 import { z } from "zod";
 
 const userSchemaValidator = z.object({
-  id: z.string().min(1, "Id is required"),
   password: z
-    .string()
-    .min(8, "Password needs more than 8 characters")
-    .max(20, "Password can not be more than 20 characters"),
-  needsPasswordChange: z.boolean().default(true).optional(),
-  role: z.enum(["student", "faculty", "admin"]),
-  status: z.enum(["in-progress", "blocked"]),
-  isDeleted: z.boolean().default(false).optional(),
+    .string({
+      invalid_type_error: "Password must be string",
+    })
+    .min(8, { message: "Password must be 8 or more characters long" })
+    .max(20, { message: "Password can not be more than 20 characters" })
+    .optional(),
 });
 
 export default userSchemaValidator;
