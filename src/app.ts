a2e4f528f88059efd2e88b378e4ec605017f3c54
Nasim Reza <nasim8382@gmail.com>
2024-05-29 21:46:15 +0600

    application routers are organised

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { StudentRoutes } from "./app/modules/student/student.route";
-import { UserRoutes } from "./app/modules/user/user.route";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import router from "./app/routes";
 const app: Application = express();
 
 // parsers
@@ -11,16 +10,15 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use("/api/v1/students", StudentRoutes);
-app.use("/api/v1/users", UserRoutes);
+app.use("/api/v1", router);
 
-const getAController = (req: Request, res: Response) => {
+const test = (req: Request, res: Response) => {
   const a = 22;
   res.send(a);
 };
+app.get("/", test);
 
-app.get("/", getAController);
-
+// global error handling
 app.use(globalErrorHandler);
 
 // not found
