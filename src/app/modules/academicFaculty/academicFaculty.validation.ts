74e92857eeb0651f613a9d3133a38c9ca3d3432e
Nasim Reza <nasim8382@gmail.com>
2024-06-06 15:27:38 +0600

    academic faculty all routes adde

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,11 +1,22 @@
 import { z } from "zod";
 
 const academicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: "Academic faculty must be string",
+  body: z.object({
+    name: z.string({
+      invalid_type_error: "Academic faculty must be string",
+    }),
+  }),
+});
+
+const updateFacultyValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      invalid_type_error: "Academic faculty must be string",
+    }),
   }),
 });
 
 export const AcademicFacultyValidations = {
   academicFacultyValidationSchema,
+  updateFacultyValidationSchema,
 };
