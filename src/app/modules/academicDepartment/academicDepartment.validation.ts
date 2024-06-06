f29f37c60650073ac912422e630fb407b1f64831
Nasim Reza <nasim8382@gmail.com>
2024-06-06 18:35:50 +0600

    academic department services added

--- src/app/modules/academicDepartment/academicDepartment.validation.ts
+++ src/app/modules/academicDepartment/academicDepartment.validation.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const createAcademicDepartmentValidationSchema = z.object({
   body: z.object({
     name: z.string({
-      invalid_type_error: "Academic faculty must be string",
+      invalid_type_error: "Academic Department must be string",
     }),
   }),
 });
