a207b325837f73dc509fd9c8d999d56989c2e7b4
Nasim Reza <nasim8382@gmail.com>
2024-06-06 19:37:00 +0600

    academic deparment routers added and validation file updated

---
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { AcademicDepartmentControllers } from "./academicDepartment.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { AcademicDepartmentValidations } from "./academicDepartment.validation";
+
+const router = express.Router();
+
+router.post(
+  "/create=academic-department",
+  validateRequest(
+    AcademicDepartmentValidations.createAcademicDepartmentValidationSchema
+  ),
+  AcademicDepartmentControllers.createAcademicDepartment
+);
+
+router.get("/", AcademicDepartmentControllers.getAllAcademicDepartments);
+
+router.get(
+  "/:departmentId",
+  AcademicDepartmentControllers.getSingleAcademicDepartment
+);
+
+router.patch(
+  "/:departmentId",
+  validateRequest(
+    AcademicDepartmentValidations.updateAcademicDepartmentValidationSchema
+  ),
+  AcademicDepartmentControllers.updateAcademicDepartment
+);
+
+export const AcademicDepartmentRouters = router;
--- src/app/modules/academicDepartment/academicDepartment.validation.ts
+++ src/app/modules/academicDepartment/academicDepartment.validation.ts
@@ -4,10 +4,29 @@ const createAcademicDepartmentValidationSchema = z.object({
   body: z.object({
     name: z.string({
       invalid_type_error: "Academic Department must be string",
+      required_error: "Name is required",
+    }),
+    academicFaculty: z.string({
+      invalid_type_error: "Academic Department must be string",
+      required_error: "Faculty is required",
+    }),
+  }),
+});
+
+const updateAcademicDepartmentValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      invalid_type_error: "Academic Department must be string",
+      required_error: "Name is required",
+    }),
+    academicFaculty: z.string({
+      invalid_type_error: "Academic Department must be string",
+      required_error: "Faculty is required",
     }),
   }),
 });
 
 export const AcademicDepartmentValidations = {
   createAcademicDepartmentValidationSchema,
+  updateAcademicDepartmentValidationSchema,
 };
