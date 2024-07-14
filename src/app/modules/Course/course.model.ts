d45b66ee3b3f2af1c1786425627afb719053660f
Nasim Reza <nasim8382@gmail.com>
2024-07-14 21:08:04 +0600

    course service file added

--- src/app/modules/Course/course.model.ts
+++ src/app/modules/Course/course.model.ts
@@ -11,29 +11,38 @@ const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
   },
 });
 
-const courseSchema = new Schema<TCourse>({
-  title: {
-    type: String,
-    unique: true,
-    trim: true,
-    required: true,
+const courseSchema = new Schema<TCourse>(
+  {
+    title: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    prefix: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    code: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    credits: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    preRequisiteCourses: [preRequisiteCoursesSchema],
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  prefix: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  code: {
-    type: Number,
-    trim: true,
-    required: true,
-  },
-  credits: {
-    type: Number,
-    trim: true,
-    required: true,
-  },
-  preRequisiteCourses: [preRequisiteCoursesSchema],
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const Course = model<TCourse>("Course", courseSchema);
