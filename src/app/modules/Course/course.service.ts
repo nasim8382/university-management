ba2531a7b7603c112582db25be797e387febde7b
Nasim Reza <nasim8382@gmail.com>
2024-07-16 00:37:07 +0600

    update pre requisite courses

--- src/app/modules/Course/course.service.ts
+++ src/app/modules/Course/course.service.ts
@@ -37,10 +37,38 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
   const updatedBasicCourseInfo = await Course.findByIdAndUpdate(
     id,
     courseRemainingData,
-    { new: true, runValidators: true }
+    {
+      new: true,
+      runValidators: true,
+    }
   );
 
-  return updatedBasicCourseInfo;
+  // check if there is any pre requisite courses to update
+  if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+    // filter out the deleted fields
+    const deletedPreRequisites = preRequisiteCourses
+      .filter((el) => el.course && el.isDeleted)
+      .map((el) => el.course);
+
+    const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(id, {
+      $pull: { preRequisiteCourses: { course: { $in: deletedPreRequisites } } },
+    });
+
+    // filter ou the new course fields
+    const newPreRequisites = preRequisiteCourses?.filter(
+      (el) => el.course && !el.isDeleted
+    );
+
+    const newPreRequisiteCourses = await Course.findByIdAndUpdate(id, {
+      $addToSet: { preRequisiteCourses: { $each: newPreRequisites } },
+    });
+  }
+
+  const result = await Course.findById(id).populate(
+    "preRequisiteCourses.course"
+  );
+
+  return result;
 };
 
 const deleteCourseFromDB = async (id: string) => {
